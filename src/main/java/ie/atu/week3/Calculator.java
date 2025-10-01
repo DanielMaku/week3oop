58b9f2b9511a05b8b6c9078f1267e7ed6e9654c8
DanielMaku-STUDENT <g00467390@atu.ie>
2025-10-01 18:39:12 +0100

    added a new method called multiply

--- src/main/java/ie/atu/week3/Calculator.java
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -9,5 +9,14 @@ public class Calculator {
     {
         return firstNum+secondNum;
     }
-}
+    public  int subtract(int firstNum, int secondNum)
+    {
+        return firstNum-secondNum;
+    }
 
+    public  int multiply(int firstNum, int secondNum)
+    {
+        return firstNum*secondNum;
+    }
+
+}
--- src/main/java/ie/atu/week3/MainApp.java
+++ src/main/java/ie/atu/week3/MainApp.java
@@ -6,19 +6,51 @@ public class MainApp
 {
     public static void main(String[] args)
     {
-        Scanner scan = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         Calculator calc = new Calculator();
 
 
         System.out.println("Please enter the first number: ");
-        int firstNum = scan.nextInt();
+        int firstNum = input.nextInt();
 
         System.out.println("Please enter the second number: ");
-        int secondNum = scan.nextInt();
+        int secondNum = input.nextInt();
 
-        Calculator calculator = new Calculator();
-        int result = calc.add(firstNum, secondNum);
+        input.nextLine();
+
+        System.out.println("Pick an operation (add,subtract, multiply, divide): ");
+        String operation = input.next();
+
+        double result = switch (operation)
+        {
+            case "add" ->
+            {
+                Calculator calculator = new Calculator();
+                yield calc.add(firstNum, secondNum);
+            }
+
+            case "subtract" ->
+            {
+                Calculator calculator = new Calculator();
+                yield calc.subtract(firstNum, secondNum);
+            }
+
+            case "multiply" ->
+            {
+                Calculator calculator = new Calculator();
+                yield calc.subtract(firstNum, secondNum);
+            }
+
+            default  ->
+            {
+                System.out.println("Invalid operation");
+                yield 0;
+            }
+
+        };
+
+
         System.out.println("The answer is: " + result);
 
 
